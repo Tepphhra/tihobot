9034977e387604e0e45e79dedbefd3c19f253fef
Mikhail Filippov <73445393+Tepphhra@users.noreply.github.com>
2026-04-06 19:24:27 +0300

    Added noise groups

--- telebob.py
+++ telebob.py
@@ -7,20 +7,101 @@ bot = telebot.TeleBot(BOT_TOKEN)
 
 user_data = {}
 
+#Группы шума
+noise_groups_sources = [
+    "Шум от транспорта",
+    "Шум от стройки",
+    "Шум от соседей",
+    "Шум от инфраструктуры",
+    "Шум от животных",
+    "Шум от предприятий и организаций"
+]
+#Источники транспотного шума
+noise_transport_sources = [
+    "Транспорт на магистралях",
+    "Громкая работа двигателей",
+    "Состояние трамвайных путей",
+    "Железнодорожный транспорт"
+]
+#Источники строительного шума
+noise_building_sources = [
+    "Работы вне разрешенного времени",
+    "Использование громкого инструмента"
+]
+#Источники шума от соседей
+noise_neighbour_sources = [
+    "Громкие бытовые приборы",
+    "Ремонтные работы вне разрешенного времени",
+    "Поведение соседей"
+]
+#Источники шума от инфраструктуры
+noise_infrastructure_sources = [
+    "Громкая работа элементов вентиляции",
+    "Громкая работа элементов жилого дома"
+]
+#Источники шума от животных
+noise_animal_sources = [
+    "Животные во дврое",
+    "Животные в квартире"
+]
+#Источники шума от предприятий и организаций
+noise_commercial_sources = [
+    "Громкая реклама",
+    "Общественное заведение",
+    "Шумное предприятие"
+]
+#Временные промежутки
+noise_time = [
+    "Ночь (23:00-07:00)",
+    "Утро (07:00-11:00)",
+    "День (11:00-18:00)",
+    "Вечер (18:00-23:00)",
+    "Только в светлое время суток",
+    "Только в темное время суток",
+    "Круглосуточно"
+]
+#Длительность шума
+noise_persistence = [
+    "Несколько дней",
+    "Несколько недель",
+    "Несколько месяцев",
+    "Год и более"
+]
+#Вызываемые шумом симптомы
+noise_symptoms = [
+    "Нарушение сна",
+    "Головные боли",
+    "Звон в ушах",
+    "Головокружение",
+    "Раздражительность"
+]
 
-noise_sources = []
-noise_time = []
-noise_persistence = []
-noise_symptoms = []
 
-
-keyboard_sources = types.InlineKeyboardMarkup(row_width=2)
+keyboard_groups_sources = types.InlineKeyboardMarkup(row_width=2)
+keyboard_transport_sources = types.InlineKeyboardMarkup(row_width=2)
+keyboard_building_sources = types.InlineKeyboardMarkup(row_width=2)
+keyboard_neighbour_sources = types.InlineKeyboardMarkup(row_width=2)
+keyboard_infrastructure_sources = types.InlineKeyboardMarkup(row_width=2)
+keyboard_animal_sources = types.InlineKeyboardMarkup(row_width=2)
+keyboard_commercial_sources = types.InlineKeyboardMarkup(row_width=2)
 keyboard_time = types.InlineKeyboardMarkup(row_width=2)
 keyboard_persistence = types.InlineKeyboardMarkup(row_width=2)
 keyboard_symptoms = types.InlineKeyboardMarkup(row_width=2)
 
-for i in noise_sources:
-    keyboard_sources.add(types.InlineKeyboardButton(i, callback_data=i))
+for i in noise_groups_sources:
+    keyboard_groups_sources.add(types.InlineKeyboardButton(i, callback_data=i))
+for i in noise_transport_sources:
+    keyboard_transport_sources.add(types.InlineKeyboardButton(i, callback_data=i))
+for i in noise_building_sources:
+    keyboard_building_sources.add(types.InlineKeyboardButton(i, callback_data=i))
+for i in noise_neighbour_sources:
+    keyboard_neighbour_sources.add(types.InlineKeyboardButton(i, callback_data=i))
+for i in noise_infrastructure_sources:
+    keyboard_infrastructure_sources.add(types.InlineKeyboardButton(i, callback_data=i))
+for i in noise_animal_sources:
+    keyboard_animal_sources.add(types.InlineKeyboardButton(i, callback_data=i))
+for i in noise_commercial_sources:
+    keyboard_commercial_sources.add(types.InlineKeyboardButton(i, callback_data=i))
 for i in noise_time:
     keyboard_time.add(types.InlineKeyboardButton(i, callback_data=i))
 for i in noise_persistence:
@@ -39,15 +120,33 @@ def start(message):
     bot.send_message(message.chat.id, 'hello world')
 
     user_id = message.chat.id
-    user_data[user_id] = {'last_request':None, 'complaint_step':'source', 'source':None, 'place':None, 'time':None, 'persistence':None, 'duration':None, 'symptoms':[]}
+    user_data[user_id] = {
+        'last_request':None,
+        'complaint_step':'source',
+        'source':None,
+        'place':None,
+        'time':None,
+        'persistence':None,
+        'duration':None,
+        'symptoms':[]
+    }
 
 @bot.message_handler(commands = ['complaint'])
 def complaint_start(message):
     user_id = message.chat.id
     if user_id not in user_data:
-        user_data[user_id] = {'last_request':'complaint', 'complaint_step':'source', 'source':None, 'place':None, 'time':None, 'persistence':None, 'duration':None, 'symptoms':[]}
+        user_data[user_id] = {
+            'last_request':'complaint',
+            'complaint_step':'source',
+            'source':None,
+            'place':None,
+            'time':None,
+            'persistence':None,
+            'duration':None,
+            'symptoms':[]
+        }
     user_data[user_id]['last_request'] = 'complaint'
-    bot.send_message(user_id, "Выберите источник шума", reply_markup=keyboard_sources)
+    bot.send_message(user_id, "Выберите источник шума", reply_markup=keyboard_groups_sources)
 
 @bot.message_handler(content_types = ['text'])
 def text(message):
